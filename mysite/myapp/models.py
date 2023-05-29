86343e0e808e4cf36b14e58ce38107eddd459ee1
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-05-29 13:57:12 -0300

    cadastro terminado

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class cadastro_usuario(models.Model):
     id_usuario = models.BigAutoField(primary_key=True)
-    nome = models.CharField(max_length=10)
+    nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     senha = models.CharField(max_length=50, blank=False, null=True)
-    cpf = models.CharField(max_length=14)
+    cpf = models.CharField(max_length=15, unique=True)
     endereco= models.CharField(max_length=150)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import cadastro_usuario
+
 # Create your views here.
 
 def screenLogin(request):
     return render(request, 'usuarios/login.html')
 
-def formCadastro(request):
-    novo_usuario = cadastro_usuario()
-    request.POST.get()
-
 def screenCadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == "GET":
+        return render(request, 'usuarios/cadastro.html')
+    else:
+        connection = cadastro_usuario()
+        cursor = connection
+
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        cpf = request.POST.get('cpf')
+        endereco = request.POST.get('endereco')
+
+        usuario = cadastro_usuario.objects.filter(cpf=cpf).first()
+
+        if usuario:
+            return HttpResponse('Já existe uma conta cadastrada com esse cpf.')
+        
+        usuario = cadastro_usuario(nome=nome, email=email, senha=senha, cpf=cpf, endereco=endereco)
+        usuario.save()
+
+        connection.close()
+        cursor.close()
+
+        return HttpResponse('Cadastro realizado com sucesso')
+    
+
+    
